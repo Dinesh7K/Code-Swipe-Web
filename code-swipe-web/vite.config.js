f89b484a323c28508cf0301523e88a6d2fcd2a92
D2C-k2.dinesh <d2c-k2.dinesh@EGM-K2-DINESH.local>
2025-01-27 13:04:35 +0530

    Daisy UI

--- code-swipe-web/vite.config.js
+++ code-swipe-web/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /Users/d2c-k2.dinesh/Documents/NODE-JS/Code-Swipe-Web/Code-Swipe-Web/code-swipe-web/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: './postcss.config.js',
+  },
+});
